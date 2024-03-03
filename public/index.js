41f3f4fcfdf27f9f07f4147d70934e2f8fa6c811
Damian <damianlucarelli1@gmail.com>
2024-03-02 20:33:48 -0500

    websiteupdate

---
+++ public/index.js
@@ -0,0 +1,13 @@
+import { getDatabase, child, get} from "/firebase/database";
+
+const db = getDatabase();
+
+get(child(db, '/client_id')).then((snapshot) => {
+    if (snapshot.exists()) {
+        console.log(snapshot.val());
+    } else {
+        console.log(snapshot);
+    }
+}).catch((error) => {
+    console.error(error);
+});
