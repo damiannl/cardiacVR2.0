d9933439e1c7fcd3382a67ee0b884aa414905858
Damian <damianlucarelli1@gmail.com>
2024-04-03 00:25:35 -0400

    Convert Script to JS
    script in index converted to index.js

--- public/index.js
+++ public/index.js
@@ -1,15 +1,7 @@
-<<<<<<< Updated upstream
 // Import the functions you need from the SDKs you need
     
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import {getDatabase, ref, child, get} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
-=======
-// Initialize Firebase
-import {getDatabase, ref, child, get} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
-
-const app = initializeApp(firebaseConfig);
-var url = "https://sso.canvaslms.com/api/lti/authorize_redirect";
->>>>>>> Stashed changes
 
 // Your web app's Firebase configuration
     
@@ -31,33 +23,12 @@ const db = getDatabase();
 
 let state = '';
 for (let i = 0; i < len; ++i) {
-<<<<<<< Updated upstream
-    state += (Math.floor(Math.random() * 16)).toString(16);
-=======
   state += (Math.floor(Math.random() * 16)).toString(16);
->>>>>>> Stashed changes
 }
 
 console.log("state:",state);
 
 get(child(ref(db), '/')).then((snapshot) => {
-<<<<<<< Updated upstream
-    if (snapshot.exists()) {
-        var val = snapshot.val();
-
-        var client_id = val.client_id;
-        var redirect_uri = val.redirect_uri;
-        var response_type = val.response_type;
-
-        url = url+'client_id='+client_id+'&response_type='+response_type+'&redirect_uri='+redirect_uri+'&state='+state;
-        console.log("url:",url);
-
-        //window.location.href = url;
-
-    } else {
-        console.log(snapshot);
-    }
-=======
   if (snapshot.exists()) {
     var val = snapshot.val();
 
@@ -73,7 +44,6 @@ get(child(ref(db), '/')).then((snapshot) => {
   } else {
     console.log(snapshot);
   }
->>>>>>> Stashed changes
 }).catch((error) => {
   console.error(error);
 });
